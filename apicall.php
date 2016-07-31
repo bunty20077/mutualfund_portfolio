0c7ce65b122b325d266df2694d4c2323618aafa2
bunty20077 <sayantan20077@rediffmail.com>
2016-07-31 16:31:49 +0530

    Date based price has been introduced

--- apicall.php
+++ apicall.php
@@ -1,6 +1,7 @@
 <?php
 
 $shareName = $_GET['shareName'];
+$requesteddate = $_GET['date'] ;
 //$shareName = "SBIN";
 
 $request =  'https://www.quandl.com/api/v3/datasets/NSE/'.$shareName.'.json?auth_token=YuPm8GJoxfFEb_aowAze'; 
@@ -10,12 +11,22 @@ $response = file_get_contents($request);
 $disp_array = array();
 $disp_array = json_decode($response,true);
 
+$date = $disp_array ["dataset"]["column_names"][0];
 
-
-echo '<TABLE BORDER="3" CELLPADDING="10" CELLSPACING="10">
+for ($i=0;$i<sizeof($disp_array ["dataset"]["data"]);$i++){
+	
+	$date = $disp_array ["dataset"]["data"][$i][0];
+	if ($requesteddate == $date ){
+		echo '<TABLE BORDER="3" CELLPADDING="10" CELLSPACING="10">
            <TD>'.$disp_array ["dataset"]["dataset_code"].'</TD>
            <TD>'.$disp_array ["dataset"]["column_names"][0].'</TD>
-           <TD>'.$disp_array ["dataset"]["data"][0][0].'</TD>
-           <TD>'.$disp_array ["dataset"]["data"][0][5].'</TD>
-           </TABLE>'
+           <TD>'.$disp_array ["dataset"]["data"][$i][0].'</TD>
+           <TD>'.$disp_array ["dataset"]["data"][$i][5].'</TD>
+           </TABLE>';
+	}
+}
+
+
+
+
 ?>
