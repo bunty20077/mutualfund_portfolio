4dc8a629e6c0eecceebedf9587b3de2c597b13f4
bunty20077 <sayantan20077@rediffmail.com>
2016-08-01 01:08:01 +0530

    NAV added

--- mutualfundapicall.php
+++ mutualfundapicall.php
@@ -6,9 +6,13 @@ Unirest\Request::verifyPeer(false);
 // Mashape auth
 Unirest\Request::setMashapeKey('96HYJG7yd2mshbmgvJoAPQIl23Khp14bMtTjsnNzZlrU0eXyqL');
 
+
+//$mutualfundName = $_GET['mutualfundName'];
+//$requesteddate = $_GET['date'] ;
+
 function latestNav(){
 	
-	$body = '{"scodes":["120503"]}';	
+	$body = '{"scodes":["120503","119564","119707","119425","119404"]}';	
 	$response = Unirest\Request::post("https://mutualfundsnav.p.mashape.com/",
 			array(
 					"Content-Type" => "application/json",
@@ -17,8 +21,9 @@ function latestNav(){
 	
 	return ($response->raw_body) ;	
 }
-function searchScript(){
-	$body = '{"search": "SBI Magnum"}';
+function searchScript($mutualfundName){
+	//$body = '{"search": "SBI Magnum"}';
+	$body = '{"search": "'.$mutualfundName.'"}';
 	$response = Unirest\Request::post("https://mutualfundsnav.p.mashape.com/",
 			array(
 					"Content-Type" => "application/json",
@@ -47,17 +52,46 @@ $disp_array = array();
 $disp_array = json_decode(latestNav(),true);
 //print_r(latestNav());
 //print_r($disp_array);
-//print_r (searchScript())."</br>";
+//print_r (searchScript("Axis Long Term Equity Fund"))."</br>";
 //echo "============================";
 //print_r (historicalNav())."</br>";
+//print_r(searchScript("L&T Gilt Fund Direct Plan"));
+//print_r(searchScript("SBI Magnum Gilt Fund - Long Term- Direct Plan - Growth"));
+//print_r(searchScript("L&T India Value Fund Direct Plan - Growth"));
+//print_r(searchScript("SBI Magnum MidCap Fund - Regular Plan-Growth"));
+//print_r(searchScript("SBI Magnum MidCap Fund - DIRECT Plan-Growth"));
 
 
-  echo '<TABLE BORDER="3" CELLPADDING="10" CELLSPACING="10">
+   echo '<TABLE BORDER="3" CELLPADDING="10" CELLSPACING="10">
            <TD>'.$disp_array ["120503"]["fund"].'</TD>
            <TD>'.$disp_array ["120503"]["nav"].'</TD>
            <TD>'.$disp_array ["120503"]["category"].'</TD>
            <TD>'.$disp_array ["120503"]["date"].'</TD>
-           </TABLE>';  
+           </TABLE>'; 
+  echo '<TABLE BORDER="3" CELLPADDING="10" CELLSPACING="10">
+           <TD>'.$disp_array ["119564"]["fund"].'</TD>
+           <TD>'.$disp_array ["119564"]["nav"].'</TD>
+           <TD>'.$disp_array ["119564"]["category"].'</TD>
+           <TD>'.$disp_array ["119564"]["date"].'</TD>
+           </TABLE>';
+  echo '<TABLE BORDER="3" CELLPADDING="10" CELLSPACING="10">
+           <TD>'.$disp_array ["119707"]["fund"].'</TD>
+           <TD>'.$disp_array ["119707"]["nav"].'</TD>
+           <TD>'.$disp_array ["119707"]["category"].'</TD>
+           <TD>'.$disp_array ["119707"]["date"].'</TD>
+           </TABLE>';
+  echo '<TABLE BORDER="3" CELLPADDING="10" CELLSPACING="10">
+           <TD>'.$disp_array ["119425"]["fund"].'</TD>
+           <TD>'.$disp_array ["119425"]["nav"].'</TD>
+           <TD>'.$disp_array ["119425"]["category"].'</TD>
+           <TD>'.$disp_array ["119425"]["date"].'</TD>
+           </TABLE>';
+  echo '<TABLE BORDER="3" CELLPADDING="10" CELLSPACING="10">
+           <TD>'.$disp_array ["119404"]["fund"].'</TD>
+           <TD>'.$disp_array ["119404"]["nav"].'</TD>
+           <TD>'.$disp_array ["119404"]["category"].'</TD>
+           <TD>'.$disp_array ["119404"]["date"].'</TD>
+           </TABLE>';
 
   
 ?>
